1f76ddd3445071900b845138a032f3f065249649
Keith Garcia <Keith@Keiths-MacBook.local>
2017-05-02 14:10:01 -0400

    dude

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -2,29 +2,101 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class MyGdxGame extends ApplicationAdapter {
+	TextureRegion down, up, left, right;
 	SpriteBatch batch;
 	Texture img;
+	boolean canJump, faceRight = true;
+
+	float x, y, xv, yv;
+	static final float MAX_VELOCITY = 500;
+	static final float MAX_JUMP_VELOCITY = 2000;
+	static final int GRAVITY = -50;
+
+	static final int WIDTH = 18;
+	static final int HEIGHT = 26;
+
+	static final int DRAW_WIDTH = WIDTH*3;
+	static final int DRAW_HEIGHT = HEIGHT*3;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("tiles.png");
+		Texture tiles = new Texture("tiles.png");
+		TextureRegion[][] grid = TextureRegion.split(tiles, 16, 16);
+		down = grid[6][0];
+		up = grid[6][1];
+		right = grid[6][3];
+		left = new TextureRegion(right);
+		left.flip(true, false);
 	}
 
-	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		move();
+
+		TextureRegion img;
+		if (y > 0) {
+			img = up;
+		}
+		else {
+			img = right;
+		}
+
+		Gdx.gl.glClearColor(0.5f, 0.5f, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		if (faceRight) {
+			batch.draw(img, x, y, DRAW_WIDTH, DRAW_HEIGHT);
+		}
+		else {
+			batch.draw(img, x + DRAW_WIDTH, y, DRAW_WIDTH * -1, DRAW_HEIGHT);
+		}
 		batch.end();
 	}
-	
+	float decelerate(float velocity) {
+		float deceleration = 0.95f; // the closer to 1, the slower the deceleration
+		velocity *= deceleration;
+		if (Math.abs(velocity) < 1) {
+			velocity = 0;
+		}
+		return velocity;
+	}
+
+	void move() {
+		if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) {
+			yv = MAX_VELOCITY;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			yv = MAX_VELOCITY * -1;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			xv = MAX_VELOCITY;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			xv = MAX_VELOCITY * -1;
+		}
+		yv += GRAVITY;
+
+		y += yv * Gdx.graphics.getDeltaTime();
+		x += xv * Gdx.graphics.getDeltaTime();
+
+		if (y < 0) {
+			y = 0;
+			canJump = true;
+		}
+
+		yv = decelerate(yv);
+		xv = decelerate(xv);
+
+	}
+
 	@Override
 	public void dispose () {
 		batch.dispose();
